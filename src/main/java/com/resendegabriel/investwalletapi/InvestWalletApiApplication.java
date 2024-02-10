3946bb155d93848e563ee1c8f37dbd6339aad29f
Gabriel Resende <gabriellrr2005@gmail.com>
2024-02-09 21:12:23 -0300

    Initial commit

---
+++ src/main/java/com/resendegabriel/investwalletapi/InvestWalletApiApplication.java
@@ -0,0 +1,13 @@
+package com.resendegabriel.investwalletapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestWalletApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvestWalletApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/resendegabriel/investwalletapi/InvestWalletApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resendegabriel.investwalletapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestWalletApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
