ab476ab3151c5ca34873c8a11be5d69fa9076a3b
Gabriel Resende <gabriellrr2005@gmail.com>
2024-02-29 21:55:10 -0300

    fix: add lombok builder annotation

--- src/main/java/com/resendegabriel/investwalletapi/domain/dto/response/reports/WalletActiveSectorsReportDTO.java
+++ src/main/java/com/resendegabriel/investwalletapi/domain/dto/response/reports/WalletActiveSectorsReportDTO.java
@@ -1,10 +1,12 @@
 package com.resendegabriel.investwalletapi.domain.dto.response.reports;
 
 import com.resendegabriel.investwalletapi.domain.dto.response.WalletSimpleDTO;
+import lombok.Builder;
 
 import java.math.BigDecimal;
 import java.util.List;
 
+@Builder
 public record WalletActiveSectorsReportDTO(WalletSimpleDTO wallet,
 
                                            BigDecimal walletTotalValue,
